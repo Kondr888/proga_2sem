583bde088cf5407d8f8d86f1f9162fb6ce3168bb
Kondr888 <53138685+Kondr888@users.noreply.github.com>
2020-06-12 21:11:08 +0300

    Add files via upload

---
+++ itmolab2/cmake-build-debug/CMakeFiles/itmolab2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/itmolab2.dir/main.cpp.o"
+  "CMakeFiles/itmolab2.dir/vector.cpp.o"
+  "itmolab2"
+  "itmolab2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/itmolab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
