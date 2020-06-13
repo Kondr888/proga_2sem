d048745eb9ee156ece2cc37fef2555aa44987b18
Kondr888 <53138685+Kondr888@users.noreply.github.com>
2020-06-13 11:37:46 +0300

    Add files via upload

---
+++ itmolab3/cmake-build-debug/CMakeFiles/itmolab3.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/aleksejkondratuk/CLionProjects/itmolab3/main.cpp" "/Users/aleksejkondratuk/CLionProjects/itmolab3/cmake-build-debug/CMakeFiles/itmolab3.dir/main.cpp.o"
+  "/Users/aleksejkondratuk/CLionProjects/itmolab3/triangle.cpp" "/Users/aleksejkondratuk/CLionProjects/itmolab3/cmake-build-debug/CMakeFiles/itmolab3.dir/triangle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ itmolab3/cmake-build-debug/CMakeFiles/itmolab3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/itmolab3.dir/main.cpp.o"
+  "CMakeFiles/itmolab3.dir/triangle.cpp.o"
+  "itmolab3"
+  "itmolab3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/itmolab3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
